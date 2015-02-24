3a459494e7590ac3977b540349b152dcf6211dfb
r0but <josht0288@gmail.com>
2015-02-23 22:57:47 -0600

    should be up to date

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 /*
  The MIT License (MIT)
- 
+
  Copyright (c) 2014 Joshua Trahan
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,8 +27,12 @@
 #include <string>
 #include <cmath>
 
-// Comment this out if you aren't using OS-X.
-#include <ResourcePath.hpp>
+#ifdef _WIN32
+//define something for Windows (32-bit and 64-bit, this part is common)
+#include "winResourcePath.hpp"
+#elif __APPLE__
+#include "ResourcePath.hpp"
+#endif
 
 #include "levelType.hpp"
 
@@ -38,7 +42,7 @@ void drawWinScreen(){
     for (int i = 0; i < 25; i++){
         cout << endl;
     }
-    
+
     cout << "Congratulate! You are winner!" << endl;
 }
 
@@ -49,22 +53,22 @@ void drawLossScreen(){
 void gameLoop(levelType level){
     char userInput;
     while(true){
-        
+
         // winOrLose - 0 means game isn't finished, 1 means win, 2 means loss
         int winOrLose = 0;
-        
+
         level.drawMap();
         level.sfmlHandler->setTextures();
         level.sfmlHandler->drawScreen();
-        
+
         userInput = level.sfmlHandler->getInput();
-        
+
         if (userInput == 'q')
             break;
         else
             winOrLose = level.iterateMap(userInput);
         cout << endl << endl;
-        
+
         if (winOrLose == 1){
             level.drawMap();
             drawWinScreen();
@@ -89,36 +93,36 @@ int main(){
             cout << "Enter the filename for the level you want to load: ";
             //cin >> levelName;
         }
-        
+
         // temporary to string the levels together
         levelName = "level" + to_string(currentLevel) + ".slv";
-        
+
         ifstream levelFile;
         string levelPath = resourcePath() + levelName;
         levelFile.open(levelPath.c_str());
-        
+
         if (!levelFile.is_open()){
             cout << "Level not found. Make sure you typed it correctly, ";
             cout << "and that it is in the game's directory." << endl << endl;
             break;
         }
-        
+
         if (currentLevel == 1){
             level = new levelType(levelFile);
         }
         else{
             level->reInitialize(levelFile);
         };
-        
+
         levelFile.close();
-        
+
         gameLoop(*level);
-        
+
         char cUserCont;
         cout << endl << "Would you like to continue, quit, or restart? "
         << "(c/q/r)";
         userCont = level->sfmlHandler->getInput();
-        
+
         if (cUserCont == 'q' || cUserCont == 'Q'){
             break;
         }
@@ -126,6 +130,6 @@ int main(){
             continue;
         }
     }
-    
+
     return 0;
 }
