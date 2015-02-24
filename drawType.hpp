7bc9cb50fff077b76126390976a49108204ebe94
r0but <josht0288@gmail.com>
2015-02-23 22:20:51 -0600

    fixing up windows

--- drawType.hpp
+++ drawType.hpp
@@ -1,18 +1,18 @@
 /*
  The MIT License (MIT)
- 
+
  Copyright (c) 2015 Joshua Trahan
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -53,42 +53,42 @@ public:
             sf::Style::Default)
     {
         pixelSize = 32;
-        
+
         vertLevelSize = nVertLevelSize;
         horizLevelSize = nHorizLevelSize;
         player = nPlayer;
         numOfEnemies = nNumOfEnemies;
-        
+
         for (int y = 0; y < 25; y++){
             for (int x = 0; x < 80; x++){
                 levelArray[x][y] = nLevelArray[x][y];
             }
 
         }
-        
+
         for (int i = 0; i < 25; i++){
             enemyArray[i] = nEnemyArray[i];
         }
-        
+
         for (int i = 0; i < 4; i++){
             if (!texPlayer[i].loadFromFile( resourcePath() + std::to_string(i) + ".png")){
                 cout << to_string(i) << " player texture failed to load\n";
             }
         }
-        
+
         for (int i = 4; i < 8; i++){
-         
+
             if (!texEnemy[i - 4].loadFromFile( resourcePath() + std::to_string(i) + ".png")){
                 cout << to_string(i) << " enemy texture failed to load\n";
             }
         }
-        
+
         for (int i = 8; i < 10; i++){
             if (!texLevel[i - 8].loadFromFile( resourcePath() + std::to_string(i) + ".png")){
                 cout << to_string(i) << " level texture failed to load\n";
             }
         }
-        
+
         // Setting tile textures
         for (int y = 0; y < 25; y++){
             for (int x = 0; x < 80; x++){
@@ -102,21 +102,21 @@ public:
                                               (float)(y * pixelSize));
             }
         }
-        
+
         return;
     }
-    
+
     char getInput(){
         sf::Event event;
-        
+
         while (window.waitEvent(event)){
-            
+
             switch(event.type){
                 case sf::Event::Closed:
                     window.close();
                     return 'q';
                     break;
-                
+
                 case sf::Event::KeyPressed:
                     if (event.key.code == sf::Keyboard::D)
                         return 'd';
@@ -129,31 +129,33 @@ public:
                     else if (event.key.code == sf::Keyboard::Q)
                         return 'q';
                     break;
-                
+
                 default:
+                    return '=';
                     break;
             };
         }
+        return '=';
     }
-    
+
     void setTextures(){
         int pX = player->getXCoord();
         int pY = player->getYCoord();
-        
+
         int enemyXCoords[numOfEnemies];
         int enemyYCoords[numOfEnemies];
-        
+
         for (int i = 0; i < numOfEnemies; i++){
             enemyXCoords[i] = enemyArray[i]->getXCoord();
             enemyYCoords[i] = enemyArray[i]->getYCoord();
         }
-        
+
         int textureNumber;
-        
+
         int playerHeading = player->getHeading();
-        
+
         cout << "playerHeading: " << playerHeading << endl;
-        
+
         switch (playerHeading){
             case 0:
                 textureNumber = 0;
@@ -172,16 +174,16 @@ public:
                 cout << "Error setting correct player texture\n";
                 break;
         };
-        
+
         spritePlayer.setTexture(texPlayer[textureNumber]);
         spritePlayer.setPosition((float)(pX * pixelSize),
                                  (float)(pY * pixelSize));
-        
+
         for (int i = 0; i < numOfEnemies; i++){
             int enemyHeading = enemyArray[i]->getHeading();
-            
+
             cout << "enemyHeading: " << enemyHeading << endl;
-            
+
             switch (enemyHeading){
                 case 0:
                     textureNumber = 0;
@@ -200,64 +202,64 @@ public:
                     cout << i << " Error setting correct enemy texture\n";
                     break;
             }
-            
+
             spriteEnemy[i].setTexture(texEnemy[textureNumber]);
             spriteEnemy[i].setPosition((float)(enemyXCoords[i] * pixelSize),
                                        (float)(enemyYCoords[i] * pixelSize));
-            
+
         }
-        
+
         // Drawing the level - make sure to only redraw a sprite or re-set a
         // texture when it needs to change, players and enemies excluded.
     }
-    
+
     int drawScreen(){
         if (window.isOpen()){
             window.clear();
-            
+
             for (int y = 0; y < 25; y++){
                 for (int x = 0; x < 80; x++){
                     window.draw(spriteLevel[x][y]);
                 }
             }
-            
+
             window.draw(spritePlayer);
-            
+
             for (int i = 0; i < numOfEnemies; i++){
                 window.draw(spriteEnemy[i]);
             }
-            
+
             window.display();
             return 0;
         }
         return 0;
     }
-    
-    
+
+
 private:
-    
+
     int pixelSize;
     int vertLevelSize;
     int horizLevelSize;
     int numOfEnemies;
     char levelArray[80][25];
     playerType *player;
-    
+
     sf::Sprite spriteArray;
-    
+
     enemyType *enemyArray[25];
-    
+
     // left right up down
     sf::Texture texEnemy[4];
     sf::Texture texPlayer[4];
-    
+
     // floor wall
     sf::Texture texLevel[2];
-    
+
     sf::Sprite spriteEnemy[25];
     sf::Sprite spritePlayer;
     sf::Sprite spriteLevel[80][25];
-    
+
     sf::RenderWindow window;
 };
 
